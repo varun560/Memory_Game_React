70f74ffe6787358dc4f28c34eca7b7c2aeda191b
Varun Jain <varunjn652@gmail.com>
2020-05-02 19:00:45 +0530

    Added Onclick

--- src/MemoryApp.js
+++ src/MemoryApp.js
@@ -39,7 +39,50 @@ class MemoryApp extends Component {
   }
 
   handleClick(id){
-    
+    const mapCardState = (cards, idsToChange, newCardState) => {
+      return cards.map(c => {
+        if (idsToChange.includes(c.id)) {
+          return {
+            ...c,
+            cardState: newCardState
+          };
+        }
+        return c;
+      });
+    }
+  
+    const foundCard = this.state.cards.find(c => c.id === id);
+  
+    if (this.state.noClick || foundCard.cardState !== CardState.HIDING) {
+      return;
+    }
+  
+    let noClick = false;
+  
+    let cards = mapCardState(this.state.cards, [id], CardState.SHOWING);
+  
+    const showingCards =  cards.filter((c) => c.cardState === CardState.SHOWING);
+  
+    const ids = showingCards.map(c => c.id);
+  
+    if (showingCards.length === 2 &&
+        showingCards[0].backgroundColor === showingCards[1].backgroundColor) {
+      cards = mapCardState(cards, ids, CardState.MATCHING);
+    } else if (showingCards.length === 2) {
+      let hidingCards = mapCardState(cards, ids, CardState.HIDING);
+  
+      noClick = true;
+  
+      this.setState({cards, noClick}, () => {
+        setTimeout(() => {
+          // set the state of the cards to HIDING after 1.3 seconds
+          this.setState({cards: hidingCards, noClick: false});
+        }, 1300);
+      });
+      return;
+    }
+  
+    this.setState({cards, noClick});
   }
 
   handleNewGame(){
